739ab42bee97b5001f6c46287edf3ebbd8fc7d20
duyongguang <yongguangdu@gmail.com>
2018-06-11 16:30:14 +0800

    'auto'

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var translate = require('./index.js');
-translate('this is China.', {from: 'en', to: 'zh-CN'}).then(result => {
+translate('this is China.', {from: 'auto', to: 'zh-CN'}).then(result => {
         console.log(result);
 }).catch(error => {
         console.log(error);
